99aa97c01f9cb39e1e85fa249f6b7b3d99efe85a
SeraviaSamuel <seravia.sameh18@gmail.com>
2024-03-25 21:31:59 +0200

    hi again

---
+++ Controllers/SeraviaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class SeraviaController : Controller
+    {
+        // GET: SeraviaController
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult index2()
+        {
+            return Content("hello again");
+        }
+
+
+    }
+}
